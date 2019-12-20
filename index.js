523b9888d84f171816dbcc9454556659de37ab42
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-20 14:26:47 +0530

    Added a feature to use and test Google Oauth 2.0 Service

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const config = require('./config/config');
+
 const app = express();
+
+passport.use(new GoogleStrategy({
+    clientID: keys.GOOGLE_CLIENT_ID,
+    clientSecret: keys.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.GOOGLE_callbackURL
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+}));
 
 app.get('/', (req, res) => {
     res.send({ hi: 'hello' });
 });
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get(config.GOOGLE_callbackURL, passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
