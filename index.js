c9855b83f5f40c9094ad2b15587e1ef6b006e24a
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-20 16:48:21 +0530

    Installed mongoose and connected to mongo db

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
 const authRoutes = require('./routes/auth-routes');
+const keys = require('./config/keys');
+
+async function connectToDb() {
+    await mongoose.connect(keys.MONGO_DB_LIVE_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+
+connectToDb().then(console.log('connected to mongo db'));
 
 const app = express();
 
@@ -12,3 +23,5 @@ authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,10 +3,12 @@ const config = require('../config/config');
 
 module.exports = (app) => {
 
+    // intiate oauth process
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }));
 
+    // authenticate use with the code google oauth provided 
     app.get(config.GOOGLE_callbackURL, passport.authenticate('google'));
 
 };
