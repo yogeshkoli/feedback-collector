8f262a80f09d0f700b391836e57d3204ae2e72be
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-20 19:51:58 +0530

    Enable cookies  and passport session

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth-routes');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -17,6 +20,14 @@ async function connectToDb() {
 connectToDb().then(console.log('connected to mongo db'));
 
 const app = express();
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.COOKIE_KEY]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.send({ hi: 'hello' });
