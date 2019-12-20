f06608a69fd6dad0ffc329ea52e89898883e20b2
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-20 22:50:07 +0530

    commit production settings for heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    MONGO_DB_LIVE_URI: process.env.MONGO_DB_LIVE_URI,
+    MONGO_DB_DEV_URI: process.env.MONGO_DB_DEV_URI,
+    COOKIE_KEY: process.env.COOKIE_KEY
+};
