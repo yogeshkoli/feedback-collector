26317fe37dfa88467f43b25dcacf73190d828a4a
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-30 16:27:34 +0530

    stripe configuration

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     MONGO_DB_LIVE_URI: process.env.MONGO_DB_LIVE_URI,
     MONGO_DB_DEV_URI: process.env.MONGO_DB_DEV_URI,
-    COOKIE_KEY: process.env.COOKIE_KEY
+    COOKIE_KEY: process.env.COOKIE_KEY,
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
 };
