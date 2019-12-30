867249f8c067b9c5d2dc86c97066236125909f77
yogeshkoli <yogeshkoli@hotmail.com>
2019-12-30 19:58:09 +0530

    Run React build files from Express server on node server

--- index.js
+++ index.js
@@ -38,7 +38,16 @@ app.get('/', (req, res) => {
 authRoutes(app);
 billingRoutes(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // making sure that express will serve up the production assets like our main.js file or main.css file.
+    app.use(express.static('client/build'));
+
+    // express will serve up index.html file if it does not recognize the route 
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
