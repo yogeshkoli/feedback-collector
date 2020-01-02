08612f3edcdba35318c3e11e2727a3ce6048098a
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-02 21:05:07 +0530

    Added Sendgrid API Key configugration in config file

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,5 @@ module.exports = {
     COOKIE_KEY: process.env.COOKIE_KEY,
     STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    SENDGRID_KEY: process.env.SENDGRID_KEY,
 };
