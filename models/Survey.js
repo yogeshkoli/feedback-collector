1e5dfb49e3af2eaaf39fd7613a1cb645ae70eb87
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-02 21:05:26 +0530

    survey model data filed update

--- models/Survey.js
+++ models/Survey.js
@@ -11,7 +11,7 @@ const surveySchema = new Schema({
     no: { type: Number, default: 0 },
     _user: { type: Schema.Types.ObjectId, ref: 'User' },
     dateSent: Date,
-    lastResponded: Date
+    lastResponded: { type: Date, default: null }
 });
 
 mongoose.model('surveys', surveySchema);
