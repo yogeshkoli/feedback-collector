ba5d3736cbec094c56a637d485657a26c49d39bb
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-02 21:06:41 +0530

    Added required credit middle to make sure user has enough credit to send surveys

--- middlewares/requireCredits.js
+++ middlewares/requireCredits.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.user.credits < 1) {
-        return res.status(403).send({ erorr: 'Not enough credits!' })
+        return res.status(403).send({ erorr: 'You do not have enough credits!' })
     }
 
     next();
