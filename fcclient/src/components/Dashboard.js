e08952fbd93c7fa4f0bd67cb129d660e82aeccc5
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-03 11:03:16 +0530

    Add Survey button

--- fcclient/src/components/Dashboard.js
+++ fcclient/src/components/Dashboard.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
                 <h1>Dashboard</h1>
+
+                <div className="fixed-action-btn">
+                    <Link className="btn-floating btn-large purple" to="/surveys/new">
+                        <i className="large material-icons">add</i>
+                    </Link>
+                </div>
             </div>
         );
     };
