6b4f9f06aa754c48ac2fd51efc781d1ef3f634f5
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-06 17:36:15 +0530

    Fixed email regular express warning

--- fcclient/src/utils/validateEmails.js
+++ fcclient/src/utils/validateEmails.js
@@ -1,4 +1,4 @@
-const regularExpress = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regularExpress = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default (emails) => {
     const invalidEmails = emails
@@ -7,7 +7,7 @@ export default (emails) => {
         .filter(email => regularExpress.test(email) === false);
 
     if (invalidEmails.length) {
-        return `These emails are invalid: ${invalidEmails}`;
+        return `${invalidEmails.length === 1 ? 'This email is: ' : 'These emails are invalid: '} ${invalidEmails}`;
     }
 
     return;
