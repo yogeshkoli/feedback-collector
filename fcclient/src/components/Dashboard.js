c0fe6c852efa7f507ccca8e7628de3f055420dea
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-09 23:11:23 +0530

    Added SurveyList component to list surveys

--- fcclient/src/components/Dashboard.js
+++ fcclient/src/components/Dashboard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SurveyList from './surveys/SurveyList';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <h1>Dashboard</h1>
+
+                <SurveyList></SurveyList>
 
                 <div className="fixed-action-btn">
                     <Link className="btn-floating btn-large purple" to="/surveys/new">
