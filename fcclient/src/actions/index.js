6caf875de4f24081c0caa724cff1d199de744157
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-09 23:10:40 +0530

    Added Redux Action Creator, action type and a Reducer itself to featch surveys

--- fcclient/src/actions/index.js
+++ fcclient/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 /**
  * Action creators for react reducer
@@ -23,4 +23,10 @@ export const submitSurvey = (values, history) => async dispatch => {
     history.push('/surveys');
 
     dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const featchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
