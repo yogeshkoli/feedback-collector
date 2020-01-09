a6da4bdd1a7bce2538e6de61200ce0a9669d3870
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-09 18:50:42 +0530

    Webhook implementation - parsing sendgrid events making events compact and unique according to the PATH

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -35,7 +38,25 @@ module.exports = (app) => {
         }
     });
 
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send('Thanks for voting!');
+    app.post('/api/surveys/webhooks', (req, res) => {
+
+        const events = _.map(req.body, ({ email, url }) => {
+            const pathname = new URL(url).pathname;
+            const p = new Path('/api/surveys/:surveyId/:choice');
+
+            const match = p.test(pathname);
+
+            if (match) {
+                return { email, surveyId: match.surveyId, choice: match.choice };
+            }
+        })
+
+        const compactEvents = _.compact(events);
+
+        const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+        console.log(uniqueEvents);
+
+        res.send({});
     });
 };
