e028f42c17b352a998a75ca92f2d15f688132cd3
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-10 19:05:19 +0530

    Redesinged Survey List

--- fcclient/src/components/surveys/SurveyList.js
+++ fcclient/src/components/surveys/SurveyList.js
@@ -2,6 +2,41 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { featchSurveys } from '../../actions';
 
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = theme =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        paper: {
+            padding: theme.spacing(1),
+            textAlign: 'center',
+            color: theme.palette.text.secondary,
+        },
+        card: {
+            minWidth: 275,
+        },
+        bullet: {
+            display: 'inline-block',
+            margin: '0 2px',
+            transform: 'scale(0.8)',
+        },
+        title: {
+            fontSize: 14,
+        },
+        pos: {
+            marginBottom: 12,
+        },
+    });
+
 class SurveyList extends Component {
 
     componentDidMount() {
@@ -10,25 +45,34 @@ class SurveyList extends Component {
 
     renderSurveys() {
 
+        const { classes } = this.props;
+
         return this.props.surveys.reverse().map(survey => {
             return (
-                <div className="card grey lighten-4" key={survey._id}>
-                    <div className="card-content ">
-                        <span className="card-title">
-                            {survey.title}
-                        </span>
-                        <p>
-                            {survey.body}
-                        </p>
-                        <p className="right">
-                            Sent On: {new Date(survey.dateSent).toLocaleDateString()}
-                        </p>
-                    </div>
-                    <div className="card-action">
-                        <span className="mr-2">Yes: {survey.yes}</span>
-                        <span>No: {survey.no}</span>
-                    </div>
-                </div>
+                <Grid item xs={4} key={survey._id}>
+
+                    <Card className={classes.card} >
+                        <CardContent>
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                Survey
+                        </Typography>
+                            <Typography variant="h5" component="h2">
+                                {survey.title}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                {survey.body}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">See Details</Button>
+                            <span className="mr-2">Yes: {survey.yes}</span>
+                            <span>No: {survey.no}</span>
+                        </CardActions>
+                    </Card>
+                </Grid>
             );
         });
 
@@ -36,9 +80,21 @@ class SurveyList extends Component {
     }
 
     render() {
+
+        const { classes } = this.props;
+
         return (
             <div>
-                {this.renderSurveys()}
+
+                <div className={classes.root}>
+                    <Grid container spacing={1}>
+                        <Grid container item xs={12} spacing={3}>
+                            {this.renderSurveys()}
+                        </Grid>
+                    </Grid>
+                </div>
+
+
             </div>
         );
     }
@@ -48,4 +104,4 @@ function mapStateToProps({ surveys }) {
     return { surveys };
 }
 
-export default connect(mapStateToProps, { featchSurveys })(SurveyList);
+export default connect(mapStateToProps, { featchSurveys })(withStyles(useStyles)(SurveyList));
