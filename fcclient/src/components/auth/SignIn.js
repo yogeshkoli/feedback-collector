db2d5eb00101b572e4735c85695516873da86364
yogeshkoli <yogeshkoli@hotmail.com>
2020-01-17 11:45:48 +0530

    Comment sign in form for now

--- fcclient/src/components/auth/SignIn.js
+++ fcclient/src/components/auth/SignIn.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import TextField from '@material-ui/core/TextField';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -96,9 +96,9 @@ class SignIn extends Component {
                         Sign In with Github
           </Button>
 
-                    <h4>OR</h4>
+                    {/* <h4>OR</h4> */}
 
-                    <form className={classes.form} noValidate>
+                    {/* <form className={classes.form} noValidate>
                         <TextField
                             variant="outlined"
                             margin="normal"
@@ -147,7 +147,7 @@ class SignIn extends Component {
                                 </Link>
                             </Grid>
                         </Grid>
-                    </form>
+                    </form> */}
                 </div>
                 <Box mt={8}>
                     {this.Copyright()}
